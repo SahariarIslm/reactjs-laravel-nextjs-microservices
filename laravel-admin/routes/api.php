34b935a585488940bf8ea7013bef3f4436fa0d78
SahariarIslm <dhruv.islam7584@gmail.com>
2026-05-23 19:05:00 +0600

    Order Items CSV export functionality has been created

--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -16,6 +16,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('user/info', [UserController::class,'updateInfo']);
     Route::put('user/password', [UserController::class,'updatePassword']);
     Route::post('upload', [ImageController::class,'upload']);
+    Route::get('export', [OrderController::class,'export']);
 
     Route::apiResource('users', UserController::class);
     Route::apiResource('roles', RoleController::class);

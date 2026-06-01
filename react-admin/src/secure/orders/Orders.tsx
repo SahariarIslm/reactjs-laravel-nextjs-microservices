17a24b81a6e39c5724d64390f359fadcf7d02218
SahariarIslm <dhruv.islam7584@gmail.com>
2026-06-01 18:33:28 +0600

    Order Export Complete

--- react-admin/src/secure/orders/Orders.tsx
+++ react-admin/src/secure/orders/Orders.tsx
@@ -23,10 +23,28 @@ export default class Orders extends Component {
         this.page = page;
         await this.componentDidMount();
     }
+
+    handleExport = async () => {
+        const response = await axios.get('export', {
+            responseType: 'blob'
+        });
+        const blob = new Blob([response.data], { type: 'text/csv' });
+        const downloadUrl = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = downloadUrl;
+        link.download = 'orders.csv';
+        link.click();
+    }
     
     render() {
         return (
             <Wrapper>
+                <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 ob-2 mb-3 border-bottom">
+                    <div className='btn-toolbar mb-2 mb-md-0'>
+                        <a onClick={this.handleExport} className='btn btn-sm btn-outline-secondary'>Export</a>
+                    </div>
+                </div>
+
                 <div className="table-responsive small"> 
                     <table className="table table-striped table-sm"> 
                         <thead> 
@@ -37,7 +55,7 @@ export default class Orders extends Component {
                                 <th scope="col">Total</th> 
                                 <th scope="col">Action</th> 
                             </tr> 
-                        </thead> 
+                        </thead>
                         <tbody> 
                             {this.state.orders.map(
                                 (order: Order) => {
@@ -56,9 +74,9 @@ export default class Orders extends Component {
                                 )
                                 }
                             )}
-                        </tbody> 
-                    </table> 
-                </div> 
+                        </tbody>
+                    </table>
+                </div>
                 <Paginator lastPage={this.last_page} handlePageChange={this.handlePageChange}    />
             </Wrapper>
         )

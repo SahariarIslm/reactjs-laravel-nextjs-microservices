febc20078f3b43134a64c4aff0d62c9c58facd59
SahariarIslm <dhruv.islam7584@gmail.com>
2026-06-14 14:26:46 +0600

    Influencer stats has been updated and user can login with stats view and revenue

--- react-influencer/src/components/Header.tsx
+++ react-influencer/src/components/Header.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { PropsWithRef, useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props: PropsWithRef<any>) => {
+    const [title,setTitle] = useState('Welcome')
+    const [description,setDescription] = useState('Share Links and earn 10% of the product price')
+    useEffect(()=>{
+        console.log(props.user);
+        if(props.user?.id){
+            setTitle('$'+props.user?.revenue)
+            setDescription('You have earned in total')
+        }
+    },[props])
+    
+    let buttons;
+    if(props.user){
+        buttons = (
+            <p>
+                <Link to={'/stats'} className="btn btn-primary my-2">Stats</Link>
+            </p>
+        );
+    }else{
+        buttons = (
+            <p>
+                <Link to={'/login'} className="btn btn-primary my-2">Login</Link>
+                <Link to={'/register'} className="btn btn-secondary my-2">Register</Link>
+            </p>
+        );
+    }
     return (
         <section className="jumbotron text-center">
             <div className="container">
-                <h1 className="jumbotron-heading">Album example</h1>
-                <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
-                <p>
-                    <a href="#" className="btn btn-primary my-2">Main call to action</a>
-                    <a href="#" className="btn btn-secondary my-2">Secondary action</a>
-                </p>
+                <h1 className="jumbotron-heading">{title}</h1>
+                <p className="lead text-muted">{description}</p>
+                {buttons}
             </div>
         </section>
     );
 };
 
-export default Header;
+export default connect((state: any) => ({user: state.user}))(Header);
